11121cc6edc0476d05360a5030aab0617b5b6ce5
Byron Ho <byronho24@gmail.com>
2019-01-09 16:40:34 +1000

    fixed signupform/views.py

--- signupform/views.py
+++ signupform/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
         context = {
-            'speaker_ids': [1, 2, 3, 4, 5]
+            'speaker_ids': [1, 2, 3, 4, 5],
         }
     return render(request, 'signupform/index.html', context)
