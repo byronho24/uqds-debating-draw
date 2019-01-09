971321103d8cf9cb02924fcf925787b10d169b4a
Byron Ho <byronho24@gmail.com>
2019-01-09 16:37:26 +1000

    fixed templating issues in signupform/index.html

--- signupform/views.py
+++ signupform/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context = {}
+        context = {
+            'speaker_ids': [1, 2, 3, 4, 5]
+        }
     return render(request, 'signupform/index.html', context)
