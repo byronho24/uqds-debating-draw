914885b1d53f9c0379c954f8dd9919e010b54ec9
Byron Ho <byronho24@gmail.com>
2019-01-09 16:43:56 +1000

    fixed indentatio for signupform/views.py

--- signupform/views.py
+++ signupform/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-        context = {
-            'speaker_ids': [1, 2, 3, 4, 5],
-        }
+    context = {
+        'speaker_ids': [1, 2, 3, 4, 5]
+    }
     return render(request, 'signupform/index.html', context)
