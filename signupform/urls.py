33110e7dcd0d154ed6c35a4a5cc629e7f487fa42
Byron Ho <byronho24@gmail.com>
2019-01-09 15:53:11 +1000

    Added html template for team signup form

--- signupform/urls.py
+++ signupform/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signupform, name='signupform'),
+    path('', views.index, name='index'),
 ]
--- signupform/views.py
+++ signupform/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def signupform(request):
-    return HttpResponse("Hi.")
+def index(request):
+    template = loader.get_template('signupform/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
