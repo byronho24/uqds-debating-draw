90a5d54c54640a0104e605aeeb5ee4e6d146c74e
Byron Ho <byronho24@gmail.com>
2019-01-11 22:26:25 +1000

    Added Attendance class

--- fixtures/models.py
+++ fixtures/models.py
@@ -13,8 +13,20 @@ class Team(models.Model):
 
 class Speaker(models.Model):
 
+    JUDGE_THRESHOLD = 10
+
+    WEIGHTS = {
+        'StateTeam': 10,
+        'Pro': 30,
+        'EastersAttend': 5,
+        'EastersBreak': 10,
+        'AustralsBreak': 20,
+        'AWDCBreak': 10,
+        'WUDCBreak': 100
+    }
+
     name = models.CharField(max_length=50, unique=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE) # TODO: fix cascade
     state_team = models.BooleanField(default=False)
     pro = models.BooleanField(default=False)
     easters_attend = models.BooleanField(default=False)
@@ -25,3 +37,22 @@ class Speaker(models.Model):
 
     def __str__(self):
         return self.name
+
+    def is_qualified_as_judge(self) -> bool:
+        score = 0
+        score += state_team * WEIGHTS['StateTeam'] + \
+                 pro * WEIGHTS['Pro'] + \
+                 easters_attend * WEIGHTS['EastersAttend'] + \
+                 easters_break * WEIGHTS['EastersBreak'] + \
+                 australs_break * WEIGHTS['AustralsBreak'] + \
+                 awdc_break * WEIGHTS['AWDCBreak'] + \
+                 wudc_break * WEIGHTS['WUDCBreak']
+        return score > self.JUDGE_THRESHOLD
+
+
+class Attendance:
+
+    timestamp = models.DateTimeField('timestamp')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    want_to_judge = models.BooleanField(default=True)
+    speakers = models.ManyToManyField(Speaker)
