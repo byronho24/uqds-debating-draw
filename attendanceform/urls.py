1b18e4860b3f5c0adab9dd264d5983e88a021b18
Byron Ho <byronho24@gmail.com>
2019-01-20 13:32:55 +1000

    Deleted apps signupform and attendanceform since they are now obsolete

--- attendanceform/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'attendanceform'
-urlpatterns = [
-    path('', views.markattendance, name='markattendance'),
-    path('result', views.result, name="result")
-]
--- signupform/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SignupformConfig(AppConfig):
-    name = 'signupform'
